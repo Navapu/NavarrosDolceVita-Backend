767bc39744232a5b9cd200383093cacf92cbb2f7
Alejandro Navarro <drojannp2@gmail.com>
2025-07-15 00:15:09 +0200

    feat: add user registration and login with bcrypt and JWT

--- db/models/index.js
+++ db/models/index.js
@@ -1,3 +1,5 @@
+// File to centralize all models into a single file so they can be used in controllers.
+// Archivo para centralizar todos los modelos en un unico archivo para ser usados por los controladores.
 export { User } from "./user.model.js"
 export { Product } from "./product.model.js"
 export { Order } from "./order.model.js"
--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 import express from 'express'
 import { connectDB } from './db/mongoose.js';
 import { PORT, BACKEND_URL } from './config/config.js';
+import userRouter from './routes/users.routes.js';
+
 const app = express();
+
+app.use(express.json());
+
+connectDB();
 
 app.get("/", (req, res) => {
     res.send("Navarro's Dolce Vita API – Ready to serve your Italian orders")
 })
 
-connectDB();
+
+
+app.use('/user', userRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running at: ${BACKEND_URL}${PORT}`)
