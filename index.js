f52f62ba6553bbc08491c20c62482fc0d19c105f
Alejandro Navarro <drojannp2@gmail.com>
2025-07-22 00:27:37 +0200

    chore: enable CORS middleware

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors';
 import { connectDB } from './db/mongoose.js';
 import { PORT, BACKEND_URL } from './config/config.js';
 import { errorMiddleware, notFoundHandler } from './middleware/error.middleware.js';
@@ -7,7 +8,10 @@ import productRouter from './routes/products.routes.js';
 import orderRouter from './routes/orders.routes.js';
 
 const app = express();
-
+app.use(cors());
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 
 connectDB();
